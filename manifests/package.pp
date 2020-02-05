d7cdc82bf2deff75df73550e2f693aadcccbd47c
Kristof Vandam <kristof.vandam@gmail.com>
2020-02-05 16:15:50 +0100

    file use sourca

--- manifests/package.pp
+++ manifests/package.pp
@@ -3,9 +3,7 @@ class caddyv2::package (
   Stdlib::HTTPSUrl $download_path = $caddyv2::params::download_path
   Stdlib::Unixpath $install_path  = $caddyv2::params::install_path
 ) {
-  archive { $install_path:
-    source => $download_path,
-    user   => 0,
-    group  => 0,
+  file { $install_path:
+    source => $download_path
   }
 }
